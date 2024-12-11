c657244270ed40e53ad748c667666955d156e549
yeonsu <bestdustn@sookmyung.ac.kr>
2024-12-12 01:06:59 +0900

    kafka-log-producer

---
+++ LogSend/src/main/java/com/example/logsend/LogSendApplication.java
@@ -0,0 +1,13 @@
+package com.example.logsend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogSendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogSendApplication.class, args);
+    }
+
+}
